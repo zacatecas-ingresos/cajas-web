d6981f6fe826911ad3b0b527f3fda32ae4a191ae
AlfLuis <perezcruzluisalfonso@gmail.com>
2017-03-31 17:59:02 -0600

    Nada

--- src/main/java/cajas/persistence/entity/TokenEntity.java
+++ src/main/java/cajas/persistence/entity/TokenEntity.java
@@ -2,7 +2,6 @@ package cajas.persistence.entity;
 
 import java.util.Date;
 
-import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;

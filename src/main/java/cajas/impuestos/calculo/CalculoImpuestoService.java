c67578333b62f7f71b7117dfa8dd2db00c3fa234
AlfLuis <perezcruzluisalfonso@gmail.com>
2017-04-06 10:27:02 -0500

    Calculo Impuesto

--- src/main/java/cajas/impuestos/calculo/CalculoImpuestoService.java
+++ src/main/java/cajas/impuestos/calculo/CalculoImpuestoService.java
@@ -5,6 +5,7 @@ import java.math.RoundingMode;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -32,18 +33,21 @@ public class CalculoImpuestoService {
 			String fechaInicio = "";
 			String fechaFin = "";
 
+			String[]fechas = new String[2];
+			
 			if (periodo.contains("-")) {
 				String meses[] = periodo.split("-");
-				fechaInicio = obtenerFecha(meses[0]);
-				fechaFin = obtenerFecha(meses[1]);
+				fechas = obtenerFecha(meses[0],fechaInicio,fechaFin,aFiscal);
+				fechaInicio = aFiscal + fechas[0];
+				fechas = obtenerFecha(meses[1],fechaInicio,fechaFin,aFiscal);
+				fechaFin = aFiscal + fechas[1];
 			} else {
-				fechaInicio = obtenerFecha(periodo);
-				fechaFin = obtenerFecha(periodo);
+				fechas = obtenerFecha(periodo,fechaInicio,fechaFin,aFiscal);
+				fechaInicio = aFiscal + fechas[0];
+				fechaFin = aFiscal + fechas[1];
 			}
-
-			fechaInicio = aFiscal + fechaInicio;
-			fechaFin = aFiscal + fechaFin;
-
+			
+						
 			tasa = tasaPorImpuesto(tipoImpuesto, fechaInicio, fechaFin);
 
 			impuesto = baseGravable.multiply(tasa);
@@ -78,50 +82,65 @@ public class CalculoImpuestoService {
 	 * @param periodo
 	 * @return
 	 */
-	public String obtenerFecha(String periodo) {
-		String fecha = "";
+	public String[] obtenerFecha(String periodo,String fechaInicio,String fechaFin,Integer aFiscal) {
+		
+		String []fechas = new String[2];
 		switch (periodo) {
 		case "ENERO":
-			fecha = Periodo.ENERO;
+			fechas[0] = Periodo.ENERO_INICIO;
+			fechas[1] = Periodo.ENERO_FIN;
 			break;
 		case "FEBRERO":
-			fecha = Periodo.FEBRERO;
+			fechas[0] = Periodo.FEBRERO_INICIO;
+			GregorianCalendar calendar =  new GregorianCalendar();
+			fechas[1] = (calendar.isLeapYear(aFiscal))?Periodo.FEBRERO_FIN_BISIESTO:Periodo.FEBRERO_FIN;
 			break;
 		case "MARZO":
-			fecha = Periodo.MARZO;
+			fechas[0] = Periodo.MARZO_INICIO;
+			fechas[1] = Periodo.MARZO_FIN;
 			break;
 		case "ABRIL":
-			fecha = Periodo.ABRIL;
+			fechas[0] = Periodo.ABRIL_INICIO;
+			fechas[1] = Periodo.ABRIL_FIN;
 			break;
 		case "MAYO":
-			fecha = Periodo.MAYO;
+			fechas[0] = Periodo.MAYO_INICIO;
+			fechas[1] = Periodo.MAYO_FIN;
 			break;
 		case "JUNIO":
-			fecha = Periodo.JUNIO;
+			fechas[0] = Periodo.JUNIO_INICIO;
+			fechas[1] = Periodo.JUNIO_FIN;
 			break;
 		case "JULIO":
-			fecha = Periodo.JULIO;
+			fechas[0] = Periodo.JULIO_INICIO;
+			fechas[1] = Periodo.JULIO_FIN;
 			break;
 		case "AGOSTO":
-			fecha = Periodo.AGOSTO;
+			fechas[0] = Periodo.AGOSTO_INICIO;
+			fechas[1] = Periodo.AGOSTO_FIN;
 			break;
 		case "SEPTIEMBRE":
-			fecha = Periodo.SEPTIEMBRE;
+			fechas[0] = Periodo.SEPTIEMBRE_INICIO;
+			fechas[1] = Periodo.SEPTIEMBRE_FIN;
 			break;
 		case "OCTUBRE":
-			fecha = Periodo.OCTUBRE;
+			fechas[0] = Periodo.OCTUBRE_INICIO;
+			fechas[1] = Periodo.OCTUBRE_FIN;
 			break;
 		case "NOVIEMBRE":
-			fecha = Periodo.NOVIEMBRE;
+			fechas[0] = Periodo.NOVIEMBRE_INICIO;
+			fechas[1] = Periodo.NOVIEMBRE_FIN;
 			break;
 		case "DICIEMBRE":
-			fecha = Periodo.DICIEMBRE;
+			fechas[0] = Periodo.DICIEMBRE_INICIO;
+			fechas[1] = Periodo.DICIEMBRE_FIN;
 			break;
 		default:
-			fecha = "";
+			fechas[0] ="01-01";
+			fechas[1] = "12-01";
 			break;
 		}
-		return fecha;
+		return fechas;
 	}
 
 }
--- src/main/java/cajas/impuestos/calculo/Periodo.java
+++ src/main/java/cajas/impuestos/calculo/Periodo.java
@@ -6,17 +6,30 @@ public class Periodo {
 	 * Periodos Mensuales
 	 * 
 	 */
-	public static final String ENERO = "-01-01";
-	public static final String FEBRERO = "-02-01";
-	public static final String MARZO = "-03-01";
-	public static final String ABRIL = "-04-01";
-	public static final String MAYO = "-05-01";
-	public static final String JUNIO = "-06-01";
-	public static final String JULIO = "-07-01";
-	public static final String AGOSTO = "-08-01";
-	public static final String SEPTIEMBRE = "-09-01";
-	public static final String OCTUBRE = "-10-01";
-	public static final String NOVIEMBRE = "-11-01";
-	public static final String DICIEMBRE = "-12-01";
+	public static final String ENERO_INICIO = "-01-01";
+	public static final String ENERO_FIN = "-01-31";
+	public static final String FEBRERO_INICIO = "-02-01";
+	public static final String FEBRERO_FIN = "-02-27";
+	public static final String FEBRERO_FIN_BISIESTO = "-02-28";
+	public static final String MARZO_INICIO = "-03-01";
+	public static final String MARZO_FIN = "-03-31";
+	public static final String ABRIL_INICIO = "-04-01";
+	public static final String ABRIL_FIN = "-04-30";
+	public static final String MAYO_INICIO = "-05-01";
+	public static final String MAYO_FIN = "-05-31";
+	public static final String JUNIO_INICIO = "-06-01";
+	public static final String JUNIO_FIN = "-06-30";
+	public static final String JULIO_INICIO = "-07-01";
+	public static final String JULIO_FIN = "-07-31";
+	public static final String AGOSTO_INICIO = "-08-01";
+	public static final String AGOSTO_FIN = "-08-31";
+	public static final String SEPTIEMBRE_INICIO = "-09-01";
+	public static final String SEPTIEMBRE_FIN = "-09-30";
+	public static final String OCTUBRE_INICIO = "-10-01";
+	public static final String OCTUBRE_FIN = "-10-31";
+	public static final String NOVIEMBRE_INICIO = "-11-01";
+	public static final String NOVIEMBRE_FIN = "-11-30";
+	public static final String DICIEMBRE_INICIO = "-12-01";
+	public static final String DICIEMBRE_FIN = "-12-31";
 
 }

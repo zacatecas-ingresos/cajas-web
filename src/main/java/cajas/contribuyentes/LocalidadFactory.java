a3cd8ca3624ae712506e87f7f22e17e8c1e97ac4
santiago <santiago@10.113.12.137>
2017-04-17 11:32:28 -0500

    Persistencia y entidades de localidad y municipio

---
+++ src/main/java/cajas/contribuyentes/LocalidadFactory.java
@@ -0,0 +1,64 @@
+package cajas.contribuyentes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cajas.persistence.entity.LocalidadEntity;
+
+
+public class LocalidadFactory {
+
+ // entityToDto
+    // Entidad a DTO
+    public LocalidadEntity dtoAEntidad(Localidad dto) {
+        return dtoAEntidad(dto, null);
+    }
+
+    public LocalidadEntity dtoAEntidad(Localidad dto, LocalidadEntity entidad) {
+
+        if(entidad == null) {
+            entidad = new LocalidadEntity();
+            entidad.setIdLocalidad(dto.getIdLocalidad());
+                    }
+
+        
+        entidad.setAbreviacionLocalidad(dto.getAbreviacionLocalidad());
+        entidad.setLocalidad(dto.getLocalidad());
+        return entidad;
+    }
+    
+    // DTO a entidad
+    
+    public Localidad entidadADto(LocalidadEntity entidad) {
+        Localidad dto = new Localidad();
+    
+    
+            
+        // Set
+        // carro.color = "azul";
+        // carro.setColor("azul");
+        
+        // Get
+        // colorCarro = carro.color;
+        // colorCarro = carro.getColor();
+        
+        
+    dto.setIdLocalidad(entidad.getIdLocalidad());
+    dto.setAbreviacionLocalidad(entidad.getAbreviacionLocalidad());
+    dto.setLocalidad(entidad.getLocalidad());
+
+    return dto;
+    }
+    
+    public List<Localidad> entidadesADtos(List<LocalidadEntity> entidades) {
+        List<Localidad> dtos = new ArrayList<>();
+        
+        for(LocalidadEntity entidad : entidades) {
+            Localidad dto = entidadADto(entidad);
+            dtos.add(dto);
+        }
+
+        return dtos;
+    }
+    
+}

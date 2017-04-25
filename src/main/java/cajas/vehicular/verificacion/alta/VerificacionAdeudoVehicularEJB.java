dcf2f2ccc59332061d3c18d91a2c38c69540f4f9
Zeus-HP\Zeus <lfertr@gmail.com>
2017-04-25 14:37:29 -0500

    avances persistir adeudo verificacion

---
+++ src/main/java/cajas/vehicular/verificacion/alta/VerificacionAdeudoVehicularEJB.java
@@ -0,0 +1,70 @@
+package cajas.vehicular.verificacion.alta;
+
+import javax.ejb.Stateless;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+
+import org.joda.time.DateTime;
+
+import cajas.exception.BusinessException;
+import cajas.persistence.entity.VerificacionAdeudoVehicularEntity;
+import cajas.persistence.query.VerificacionAdeudoVehicularQuery;
+import cajas.persistence.query.VerificacionVehicularQuery;
+
+
+
+@Stateless
+public class VerificacionAdeudoVehicularEJB {
+	
+
+	@Inject
+	VerificacionAdeudoVehicularQuery vVehicularQuery; 
+	
+	@PersistenceContext(name = "sitDS")
+	private EntityManager entityManager;
+	
+	public void creaVerificacionAdeudoVehicular(VerificacionAdeudoVehicular verificacionAdeudoVehicular ){
+		
+		VerificacionAdeudoVehicularEntity verificacionAdeudoVehicularEntity = new VerificacionAdeudoVehicularEntity();
+		verificacionAdeudoVehicularEntity.setIdVerificacionVehicular(verificacionAdeudoVehicular.getIdVerificacionVehiculo());
+		verificacionAdeudoVehicularEntity.setAnio0VerificacionAdeudo(verificacionAdeudoVehicular.getAnio0VerificacionAdeudo());
+		verificacionAdeudoVehicularEntity.setAnio1VerificacionAdeudo(verificacionAdeudoVehicular.getAnio1VerificacionAdeudo());
+		verificacionAdeudoVehicularEntity.setAnio2VerificacionAdeudo(verificacionAdeudoVehicular.getAnio2VerificacionAdeudo());
+		verificacionAdeudoVehicularEntity.setAnio3VerificacionAdeudo(verificacionAdeudoVehicular.getAnio3VerificacionAdeudo());
+		verificacionAdeudoVehicularEntity.setAnio4VerificacionAdeudo(verificacionAdeudoVehicular.getAnio4VerificacionAdeudo());
+		verificacionAdeudoVehicularEntity.setAnio5VerificacionAdeudo(verificacionAdeudoVehicular.getAnio5VerificacionAdeudo());
+		verificacionAdeudoVehicularEntity.setFolioVerificacionAdeudo(verificacionAdeudoVehicular.getFolioVerificacionAdeudo());
+		verificacionAdeudoVehicularEntity.setProcedencia(verificacionAdeudoVehicular.getProcedencia());
+		verificacionAdeudoVehicularEntity.setIdEstado(verificacionAdeudoVehicular.getIdEstado());
+		verificacionAdeudoVehicularEntity.setFolioCalcamonia(verificacionAdeudoVehicular.getFolioCalcamonia());
+		verificacionAdeudoVehicularEntity.setDocumento(verificacionAdeudoVehicular.getDocumento());
+		verificacionAdeudoVehicularEntity.setEstatus(verificacionAdeudoVehicular.getEstatus());
+		verificacionAdeudoVehicularEntity.setObservaciones(verificacionAdeudoVehicular.getObservaciones());
+		verificacionAdeudoVehicularEntity.setBaja(verificacionAdeudoVehicular.getBaja());
+		verificacionAdeudoVehicularEntity.setBajaPlaca(verificacionAdeudoVehicular.getBajaPlaca());
+		
+		try {
+			SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
+			Date date = formatter.parse(verificacionAdeudoVehicular.getFechaRegularizacion());
+			verificacionAdeudoVehicularEntity.setFechaRegularizacion(date);
+		} catch (ParseException  e) {
+			 e.printStackTrace();
+		}
+
+		verificacionAdeudoVehicularEntity.setFechaVerificacionAdeudo(DateTime.now().toDate());
+		
+		vVehicularQuery.registrarVerificacion(verificacionAdeudoVehicularEntity);
+						
+	}
+}

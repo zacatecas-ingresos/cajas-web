41207fbf35b0a980682f59b2db7e8753ca3ac50a
kartegen <kartegen@gmail.com>
2017-04-28 16:46:11 -0500

    Estatus Verificacion

--- src/main/java/cajas/vehicular/verificacion/estatusverificacion/EstatusVerificacionEJB.java
+++ src/main/java/cajas/vehicular/verificacion/estatusverificacion/EstatusVerificacionEJB.java
@@ -2,4 +2,8 @@ package cajas.vehicular.verificacion.estatusverificacion;
 
 public class EstatusVerificacionEJB {
 
+	public EstatusVerificacion obtenerEstatusVerificacionPorId(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- src/main/java/cajas/vehicular/verificacion/estatusverificacion/EstatusVerificacionResources.java
+++ src/main/java/cajas/vehicular/verificacion/estatusverificacion/EstatusVerificacionResources.java
@@ -1,5 +1,25 @@
 package cajas.vehicular.verificacion.estatusverificacion;
 
-public class EstatusVerificacionResources {
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import cajas.exception.BusinessException;
+
+public class EstatusVerificacionResources {
+	@Inject
+	private EstatusVerificacionEJB estatusVerificacionEJB;
+	@GET
+	@Produces({ "application/json" })
+	public Response obtenerEstatusVerificacionPorId(@QueryParam("idEstatusVerificacion")Integer id) {
+		try {
+			EstatusVerificacion estatusverificacion = estatusVerificacionEJB.obtenerEstatusVerificacionPorId(id);
+			return Response.ok(estatusverificacion).build();
+		} catch (BusinessException ex) {
+			return Response.status(Status.NOT_IMPLEMENTED).tag(ex.getMessage()).build();
+		}
+	}
 }

9ba8a98df80e002bcd942b4a807b25df24d5c5d6
Leila Schiaffini Ehuan <leila.schiaffini@gmail.com>
2017-05-04 14:01:35 -0500

    Estatus Presupuesto
    Numeración de los estatus del presupuesto

--- src/main/java/cajas/impuestos/presupuesto/EstatusPresupuesto.java
+++ src/main/java/cajas/impuestos/presupuesto/EstatusPresupuesto.java
@@ -6,6 +6,9 @@ public class EstatusPresupuesto {
 
 	}
 
-	public static final int GENERADO = 1;
+	public static final int ESPERA_CONFIRMACION_PAGO = 1; //Cuando el contribuyente ha expedido su presupuesto para pagar en ventanilla.
+	public static final int CANCELADO = 2; //Para generar otro presupuesto o por que lo rechazo el banco.
+	public static final int PROCESO_PAGO = 3;//Cuando se realizó un pago en línea 
+	
 
 }

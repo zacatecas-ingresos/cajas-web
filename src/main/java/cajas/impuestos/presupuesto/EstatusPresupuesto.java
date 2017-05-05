3b10193c409fd4745985085f7a2ade523bbffa40
edgar <edgar@192.168.1.74>
2017-05-04 22:56:59 -0500

    Modificacion de estatus

--- src/main/java/cajas/impuestos/presupuesto/EstatusPresupuesto.java
+++ src/main/java/cajas/impuestos/presupuesto/EstatusPresupuesto.java
@@ -6,11 +6,14 @@ public class EstatusPresupuesto {
 
 	}
 
-	public static final int ESPERA_CONFIRMACION_PAGO = 1; //Cuando el contribuyente ha expedido su presupuesto para pagar en ventanilla.
-
-	public static final int CANCELADO = 2; //Para generar otro presupuesto
-
-	public static final int PROCESO_PAGO = 3;//Cuando se realizó un pago en línea 
+	public static final int ESPERA_CONFIRMACION_PAGO = 1; //Cuando se hace una transaccion en linea se genera este estatus al momento de establecer conexion con banco.
+	public static final int CANCELADO = 2; //
+	public static final int RESPONDIDO = 3;//Cuando el banco responde de manera positiva y se espera la confirmacion de transaccion via integracion 
 	
+	public static final int VENTANILLA = 4; //Se usa para identificar calculos que se usaron para realizar pagos via ventanilla bancaria.
+	public static final int RECHAZADO = 5;  // cuando el banco rechaza la transaccion.
+	public static final int INTEGRADO = 6; // cuando el presupuesto se encuentra ya integrado.
+
 
 }
+

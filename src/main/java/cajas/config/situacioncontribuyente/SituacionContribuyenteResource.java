5bb52b1d6f8897a189f7970fad502228c8923b0c
alex1pm <degel.ushiha@gmail.com>
2017-05-10 10:10:16 -0500

    correcccion en SituacionContribuyenteResource

--- src/main/java/cajas/config/situacioncontribuyente/SituacionContribuyenteResource.java
+++ src/main/java/cajas/config/situacioncontribuyente/SituacionContribuyenteResource.java
@@ -25,7 +25,7 @@ import javax.ws.rs.core.MediaType;
  *
  * @author Alejandro Perez
  */
-@Path("ituacionContribuyente")
+@Path("situacionContribuyente")
 public class SituacionContribuyenteResource {
     @EJB
     private SituacionContribuyenteEJB situacionContribuyenteEJB;
